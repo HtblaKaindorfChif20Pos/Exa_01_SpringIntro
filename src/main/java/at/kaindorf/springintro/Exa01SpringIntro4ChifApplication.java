e7fc964493aa3971694e96fe758fc0527f1a6e94
Heinz Schiffermueller <sf@htlkaindorf.at>
2023-09-14 10:23:47 +0200

    Projet setup

---
+++ src/main/java/at/kaindorf/springintro/Exa01SpringIntro4ChifApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.springintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exa01SpringIntro4ChifApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Exa01SpringIntro4ChifApplication.class, args);
+  }
+
+}
---
+++ src/test/java/at/kaindorf/springintro/Exa01SpringIntro4ChifApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exa01SpringIntro4ChifApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
